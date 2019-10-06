d6dbab8499a2eb422347b062db8b772a428258a2
vrbka <vrbka@LAPTOP-BDIK3IMO>
2019-10-06 14:41:29 +0200

    Renaming and created initializer

---
+++ StockSystem/StockSystem/RestaurantManagerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Runtime.InteropServices.ComTypes;
+using DAL;
+using DAL.Models;
+
+namespace StockSystem
+{
+    public class RestaurantManagerDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemAmount> ItemAmounts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<PaymentInfo> PaymentInfos { get; set; }
+
+        public RestaurantManagerDbContext() : base("RestaurantManager")
+        {
+            Database.SetInitializer(new RestaurantManagerInitializer());
+        }
+    }
+}
---
+++ StockSystem/StockSystem/RestaurantManagerInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using StockSystem;
+
+namespace DAL
+{
+    class RestaurantManagerInitializer : DropCreateDatabaseAlways<RestaurantManagerDbContext>
+    {
+        protected override void Seed(RestaurantManagerDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
