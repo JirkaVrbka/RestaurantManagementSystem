80d7beae0a0a78c7923a7e6dfc9c4385c48723cc
jirka <vrbka.jirka@gmail.com>
2019-10-29 19:35:55 +0100

    Company service basic implementation

--- StockSystem/RestaurantManager.BussinessLayer/Services/CompanyService.cs
+++ StockSystem/RestaurantManager.BussinessLayer/Services/CompanyService.cs
@@ -4,6 +4,8 @@ using RestaurantManager.BussinessLayer.QueryObjects;
 using RestaurantManager.BussinessLayer.Services.Common;
 using RestaurantManager.DAL.Models;
 using RestaurantManager.Infrastructure;
+using RestaurantManager.Infrastructure.EntityFramework;
+using RestaurantManager.Infrastructure.EntityFramework.UnitOfWork;
 using RestaurantManager.Infrastructure.Query;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,7 @@ namespace RestaurantManager.BussinessLayer.Services
 {
     class CompanyService<TEntity, TDto, TFilterDto> : CrudQueryServiceBase<TEntity, TDto, TFilterDto>
         where TEntity : Company, new()
+        // TODO use full companyDto (for create)
         where TDto : CompanyIdDto, new()
         where TFilterDto : FilterDtoBase, new()
     {
@@ -22,9 +25,17 @@ namespace RestaurantManager.BussinessLayer.Services
         {
         }
 
-        protected override Task<TEntity> GetWithIncludesAsync(int entityId)
+        protected override async Task<TEntity> GetWithIncludesAsync(int entityId)
         {
-           
+
+            return await Repository.GetAsync(entityId, new string[] { "Persons", "Roles", "Items" });
+        }
+
+        protected  async Task<CompanyUsersDto> GetWithIncludesPersonAsync(int entityId)
+        {
+
+            var entity = await Repository.GetAsync(entityId, new string[] { "Persons" });
+            return Mapper.Map<CompanyUsersDto>(entity);
         }
     }
 }
