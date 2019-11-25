94d3e3d48e0d1ce80c3e4d26c17115aca1f1422f
Jirka <vrbka.jirka@gmail.com>
2019-11-25 23:28:52 +0100

    Mapping added

--- StockSystem/RestaurantManager.BussinessLayer/Config/MappingConfig.cs
+++ StockSystem/RestaurantManager.BussinessLayer/Config/MappingConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using RestaurantManager.BusinessLayer.DataTransferObjects;
+using RestaurantManager.BusinessLayer.DataTransferObjects.Dtos;
 using RestaurantManager.BusinessLayer.DataTransferObjects.Filters;
 using RestaurantManager.DAL.Enums;
 using RestaurantManager.DAL.Models;
@@ -12,9 +13,13 @@ namespace RestaurantManager.BusinessLayer.Config
         {
             config.CreateMap<Company, CompanyIdDto>().ReverseMap();
             config.CreateMap<Company, CompanyUsersDto>().ReverseMap();
+            config.CreateMap<Company, CompanyCreateDto>().ReverseMap();
+            config.CreateMap<Company, CompanyWithPeopleDto>().ReverseMap();
             config.CreateMap<Company, CompanyUsersFilterDto>().ReverseMap();
+            config.CreateMap<MenuItemAmount, MenuItemAmountDto>().ReverseMap();
+            config.CreateMap<MenuItem, MenuItemDto>().ReverseMap();
             config.CreateMap<Payment, PaymentDto>().ReverseMap();
-            config.CreateMap<Role, RoleCreateDto>().ReverseMap();
+            config.CreateMap<Person, PersonCreateDto>().ReverseMap();
             config.CreateMap<Person, PersonDto>().ReverseMap();
             config.CreateMap<Person, PersonGetCompanyDto>().ReverseMap();
             config.CreateMap<StockItem, ItemSellingInfoDto>().ReverseMap();
