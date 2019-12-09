903d9403066f82512d8b9106bd47dd433e2b23a6
Jirka <vrbka.jirka@gmail.com>
2019-12-09 05:35:47 +0100

    Simple EF test

---
+++ RestaurantManager/RestaurantManager.Infrastructure.EF.Test/Config/EFTestInstaller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using RestaurantManager.DAL;
+using RestaurantManager.DAL.Models;
+using RestaurantManager.Infrastructure.EF.UnitOfWork;
+using RestaurantManager.Infrastructure.Query;
+using RestaurantManager.Infrastructure.UnitOfWork;
+
+namespace RestaurantManager.Infrastructure.EF.Test.Config
+{
+    public class EFTestInstaller : IWindsorInstaller
+    {
+        private const string TestDbConnectionString = "RestaurantManagerEFTest";
+
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<Func<DbContext>>()
+                    .Instance(InitializeDatabase)
+                    .LifestyleTransient(),
+                Component.For<IUnitOfWorkProvider>()
+                    .ImplementedBy<EFUnitOfWorkProvider>()
+                    .LifestyleSingleton(),
+                Component.For(typeof(IRepository<>))
+                    .ImplementedBy(typeof(EFRepository<>))
+                    .LifestyleTransient(),
+                Component.For(typeof(IQuery<>))
+                    .ImplementedBy(typeof(EFQuery<>))
+                    .LifestyleTransient()
+            );
+        }
+
+        private static DbContext InitializeDatabase()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<RestaurantManagerDbContext>());
+            RestaurantManagerDbContext context = new RestaurantManagerDbContext(TestDbConnectionString);
+
+            var company = new Company
+            {
+                Id = 0,
+                Ico = 12345678,
+                Name = "Panda",
+                Employees = new List<Employee>(),
+                JoinDate = DateTime.Now,
+                MenuItems = new List<MenuItem>(),
+                Orders = new List<Order>(),
+                Payments = new List<Payment>(),
+                Stock = new List<Stock>()
+            };
+
+            context.Companies.Add(company);
+            context.SaveChanges();
+            return context;
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Infrastructure.EF.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestaurantManager.Infrastructure.EF.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestaurantManager.Infrastructure.EF.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b9e29a3-0dab-43b3-bacb-7cdfad243bce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
