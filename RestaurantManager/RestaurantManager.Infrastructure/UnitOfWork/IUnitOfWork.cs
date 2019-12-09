f196bba881a98abed99a48909ac4a11e0596a243
Jirka <vrbka.jirka@gmail.com>
2019-12-09 03:16:44 +0100

    Unit of work interfaces

---
+++ RestaurantManager/RestaurantManager.Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Infrastructure.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Persists all changes made within this unit of work.
+        /// </summary>
+        Task Commit();
+
+        /// <summary>
+        /// Registers an action, which is executed if and only if commit is succesfull.
+        /// </summary>
+        void RegisterAction(Action action);
+    }
+}
