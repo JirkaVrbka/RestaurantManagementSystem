721d9054d0332c7d2fbf4fa083800b5803dff508
David <d.seckar@gmail.com>
2019-12-10 07:52:04 +0100

    some required annmotations added

--- RestaurantManager/RestaurantManager.BusinessLayer/DTOs/DTOs/EmployeeDto.cs
+++ RestaurantManager/RestaurantManager.BusinessLayer/DTOs/DTOs/EmployeeDto.cs
@@ -1,11 +1,18 @@
-﻿using RestaurantManager.Utils.EntityEnums;
+﻿using System.ComponentModel.DataAnnotations;
+using RestaurantManager.Utils.EntityEnums;
 
 namespace RestaurantManager.BusinessLayer.DTOs.DTOs
 {
     public class EmployeeDto : DtoBase
     {
+        [Required]
+        [MaxLength(256)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(256)]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
