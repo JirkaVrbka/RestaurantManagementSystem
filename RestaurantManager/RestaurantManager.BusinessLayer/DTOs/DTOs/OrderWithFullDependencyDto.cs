f60a9b4395d768c41cd80971b9b0556e77cd0ea4
Jirka <vrbka.jirka@gmail.com>
2020-01-15 00:00:18 +0100

    Dto for order with dependencies

--- RestaurantManager/RestaurantManager.BusinessLayer/DTOs/DTOs/OrderWithFullDependencyDto.cs
+++ RestaurantManager/RestaurantManager.BusinessLayer/DTOs/DTOs/OrderWithFullDependencyDto.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RestaurantManager.BusinessLayer.DTOs.DTOs
 {
-    public class OrderWithOrderItemDepDto : DtoBase
+    public class OrderWithFullDependencyDto : DtoBase
     {
         public int CompanyId { get; set; }
         public DateTime OrderStartTime { get; set; }
